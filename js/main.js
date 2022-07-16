120363304e615f6a962815e7fe1a68a7fd7b67ad
namotoy <namotopet0615@gmail.com>
2022-07-16 21:55:58 +0900

    JSでの画像表示

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+{
+  const images = [
+    'img/010.jpg',
+    'img/011.jpg',
+    'img/012.jpg',
+    'img/013.jpg',
+    'img/014.jpg',
+    'img/015.jpg',
+    'img/016.jpg',
+    'img/017.jpg',
+    'img/018.jpg',
+    'img/019.jpg',
+    'img/020.jpg',
+    'img/021.jpg',
+    'img/022.jpg',
+    'img/023.jpg',
+    'img/024.jpg',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('main');
+  mainImage.src = images[currentIndex];
+}
